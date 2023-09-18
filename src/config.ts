7556abc1b13c982d7afc517fa5454a7ad5ac63a5
Diego <diegopsilvavrb@gmail.com>
2023-09-18 15:33:57 -0300

    fixing path

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const config = {
             URL: process.env.DB_CLOUD_CONNECTION,
         },
     },
-    DOWNLOADS_PATH: path.join(__dirname, 'tmp/downloads'),
-    UPLOADS_PATH: path.join(__dirname, 'tmp/uploads'),
+    DOWNLOADS_PATH: path.join(__dirname, '../tmp/downloads'),
+    UPLOADS_PATH: path.join(__dirname, '../tmp/uploads'),
     APP_URL: process.env.APP_URL,
 }
