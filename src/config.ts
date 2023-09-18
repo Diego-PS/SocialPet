9c69327fbf726ae88ab0c854673d5b5964409063
Diego <diegopsilvavrb@gmail.com>
2023-09-18 15:29:16 -0300

    using path

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 
@@ -12,7 +13,7 @@ export const config = {
             URL: process.env.DB_CLOUD_CONNECTION,
         },
     },
-    DOWNLOADS_PATH: 'tmp/downloads',
-    UPLOADS_PATH: 'tmp/uploads',
+    DOWNLOADS_PATH: path.join(__dirname, 'tmp/downloads'),
+    UPLOADS_PATH: path.join(__dirname, 'tmp/uploads'),
     APP_URL: process.env.APP_URL,
 }
