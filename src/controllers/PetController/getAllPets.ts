e9670d0775deb40489d8ed964f895d83ffd51c68
Diego <diegopsilvavrb@gmail.com>
2023-10-11 21:32:57 -0300

    hiding id information from get routes

--- src/controllers/PetController/getAllPets.ts
+++ src/controllers/PetController/getAllPets.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express'
 import { IPagination, Pagination } from '../../abstractions/Pagination'
 import { ErrorBody } from '../../types/ErrorBody'
-import { IPet } from '../../interfaces/IPet'
 import { Pet } from '../../entities/Pet'
+import { IPetInfo } from './types'
+import { getPetInfo } from './utils'
 
 interface GetAllPetsReqBody {
     pagination?: IPagination,
@@ -10,12 +11,8 @@ interface GetAllPetsReqBody {
 
 export type GetAllPetsRequest = Request<{}, {}, GetAllPetsReqBody>
 
-export interface IPetWithUrl extends Omit<IPet, 'id' | 'petId'> {
-    profilePicUrl?: string
-}
-
 interface GetAllPetsResBody {
-    pets: IPetWithUrl[]
+    pets: IPetInfo[]
 }
 
 export type GetAllPetsResponse = Response<ErrorBody | GetAllPetsResBody>
@@ -25,22 +22,10 @@ export const getAllPets = async (req: GetAllPetsRequest, res: GetAllPetsResponse
     try {
         const pagination = req.body.pagination ? new Pagination(req.body.pagination) : undefined
         const pets = await Pet.getAll(pagination)
-        const petsWithUrl = await getPetsWithUrl(pets)
-        return res.status(200).send({ pets: petsWithUrl })
+        const petInfoPromises = pets.map(pet => getPetInfo(pet))
+        const petInfos = await Promise.all(petInfoPromises)
+        return res.status(200).send({ pets: petInfos })
     } catch (err) {
         if (err instanceof Error) res.status(500).send({ error: err.message ?? 'Something went wrong' })
     }
-}
-
-const getPetsWithUrl = async (pets: Pet[]) => 
-{
-    const petsWithUrl: IPetWithUrl[] = []
-    for (const pet of pets) {
-        const { url } = await pet.downloadProfilePic()
-        petsWithUrl.push({ 
-            ...pet,
-            profilePicUrl: url
-        })
-    }
-    return petsWithUrl
 }
--- src/controllers/PostController/utils.ts
+++ src/controllers/PostController/utils.ts
@@ -1,7 +1,7 @@
 import { Post } from '../../entities/Post'
 import { IPost } from '../../interfaces/IPost'
 
-export interface IPostWithUrl extends Omit<IPost, 'id' | 'petId'> {
+export interface IPostWithUrl extends Omit<IPost, 'id' | 'petId' | 'mediaFileId'> {
     mediaUrl?: string
 }
 
@@ -10,10 +10,12 @@ export const getPostsWithUrl = async (posts: Post[]) =>
     const postsWithUrl: IPostWithUrl[] = []
     for (const post of posts) {
         const { url } = await post.download()
-        postsWithUrl.push({ 
-            ...post,
-            mediaUrl: url
-        })
+        const postWithUrl: IPostWithUrl = {
+            createdUTCDateTime: post.createdUTCDateTime,
+            mediaUrl: url,
+            textContent: post.textContent,
+        }
+        postsWithUrl.push(postWithUrl)
     }
     return postsWithUrl
 }
