65124cd6f00bee1c1429680a853d2dbcd659d1da
Diego-PS <diegopsilvavrb@gmail.com>
2023-11-02 11:59:12 -0300

    most complex function

--- src/controllers/PostController/createPost.ts
+++ src/controllers/PostController/createPost.ts
@@ -24,12 +24,16 @@ export const createPost = async (req: CreatePostRequest, res: CreatePostResponse
         if (!req.file?.path) {
             return res.status(500).send({ error: 'Upload was unsuccessful' })
         }
+
+        // add extension to filename
         const filePath = req.file.path
         const extension = req.file.originalname.split('.').at(-1)
         const newName = `${req.file.filename}.${extension}`
         await fs.promises.rename(filePath, `${filePath}.${extension}`)
+
         const pet = await Pet.getByPublicId(req.body.petPublicId)
         const post = await pet.createPost({ textContent: req.body.textContent, mediaFileId: newName })
+
         return res.status(200).send({ id: post.id, fileId: post.mediaFileId, textContent: post.textContent })
     } catch (err) {
         if (err instanceof Error) res.status(500).send({ error: err.message ?? 'Something went wrong' })
