dd88df0441b2e53ab32d7e2888aa1a5d5c0dd799
Diego <diegopsilvavrb@gmail.com>
2023-11-26 13:16:05 -0300

    changing delete route to use nickname

--- backend/src/routes/petRoutes.ts
+++ backend/src/routes/petRoutes.ts
@@ -5,7 +5,7 @@ import { controllers } from '../controllers'
 export const petRoutes = Router()
 
 petRoutes.route('/create').post(multer({ dest: 'tmp/uploads/' }).single('profilePicture'), controllers.pet.createPet)
-petRoutes.route('/delete/:id').delete(controllers.pet.deletePet)
+petRoutes.route('/delete/:nickname').delete(controllers.pet.deletePet)
 petRoutes.route('/get').get(controllers.pet.getAllPets)
 petRoutes.route('/get/:nickname').get(controllers.pet.getPet)
 petRoutes.route('/posts/:nickname').get(controllers.pet.getPostsFromPets)
